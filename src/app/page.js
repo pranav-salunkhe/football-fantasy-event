d50f9309c0bb74479b8b08175108e41c170b9c4d
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2024-03-18 06:56:39 +0530

    final

--- src/app/page.js
+++ src/app/page.js
@@ -75,8 +75,8 @@ export default function Home() {
         <div className='w-16 h-16 flex justify-center items-start bg-error rounded-full relative right-28 bottom-32'>     
         </div>
         <div className='flex flex-col w-full text-gray-600 justify-center  text-7xl'>
-            <p className='flex justify-center items-center w-full border-[1px]  skew-x-6 border-secondary bg-secondary' style={{fontWeight:800}}>ISTE</p>
-            <p className='flex justify-center items-center text-center border-[1px]  skew-y-6 border-error bg-error w-full' style={{fontWeight:800}}>Moneyball 24</p>
+            <p className='flex justify-center items-center w-full border-[1px]  skew-x-6 border-secondary bg-secondary' style={{fontWeight:900}}>ISTE</p>
+            <p className='flex justify-center items-center text-center border-[1px]  skew-y-6 border-error bg-error w-full' style={{fontWeight:900}}>Moneyball 24</p>
         </div>
         <div className='flex flex-col justify-center items-center p-4'>
             <div className='text-lg mt-4 text-success font-bold'><p>ISTE Credit Presents</p></div>

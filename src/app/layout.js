670ff33131c8d65f5a9425a8b85496b7d75cdc30
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2024-03-18 07:26:47 +0530

    final

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,18 +3,21 @@ import "./globals.css";
 import Nav from "../components/Nav";
 import NewNav from "@/components/NewNav";
 import Footer from "@/components/Footer";
-
+import { Roboto } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ISTE Moneyball",
   description: "Things are gonna be a lil Messi",
 };
-
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight:['100', '300', '400', '500', '700', '900']
+})
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
       <NewNav />
       <div className="min-h-screen bg-base-100 flex justify-center items-center p-4">
       {children}
